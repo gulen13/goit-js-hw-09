6b42fc8cbab7439515d03c48915d2d3837d9df96
gulen13 <vitaliy.gulen@gmail.com>
2023-03-16 22:48:41 +0200

    1 task is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
+const body = document.querySelector('body');
+let timerId = null;
+btnStop.setAttribute("disabled", "");
 
+btnStart.addEventListener('click', () => {
+  onButtonStart();
+  btnStart.setAttribute("disabled", "");
+  btnStop.removeAttribute("disabled");
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStop.setAttribute("disabled", "");
+  btnStart.removeAttribute("disabled");
+});
+
+function onButtonStart() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
